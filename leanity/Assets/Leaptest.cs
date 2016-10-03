4192e245230f7340128f2e0532442b14e0b57497
Jonatan <jonatan.martinez@neurodigital.es>
2016-10-03 13:55:18 +0200

    Initial commit. Spacemouse and leap integrated in the same project.

---
+++ leanity/Assets/Leaptest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class Leaptest : MonoBehaviour {
+
+	Controller c;
+	// Use this for initialization
+	void Start () {
+		c = new Controller();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = c.Frame();
+		Hand h;
+		if(frame.Hands.Count > 0) {
+			h = frame.Hands[0];
+			transform.rotation = lpToUnityRot(h.Rotation);
+			transform.localPosition = lpToUnityVec(h.PalmPosition);
+		}
+	}
+
+
+	Vector3 lpToUnityVec(Vector lv) {
+		const float scaleFactorX = 0.001f;
+		const float scaleFactorY = 0.001f;
+		const float scaleFactorZ = -0.001f;
+		return new Vector3(lv.x * scaleFactorX, lv.y * scaleFactorY, lv.z * scaleFactorZ);
+
+	}
+	Quaternion lpToUnityRot(LeapQuaternion lq) {
+		return new Quaternion(-lq.x, -lq.y, lq.z, lq.w);
+	}
+}
