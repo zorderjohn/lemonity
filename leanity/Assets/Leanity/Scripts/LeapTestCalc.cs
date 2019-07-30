ef7ef492a23324e5917db81a88df2dc66f0ecfb0
Jonatan Martinez <jonatan.m@gmail.com>
2019-07-30 20:36:49 +0200

    Refactor to help implementing two hand gestures

--- leanity/Assets/Leanity/Scripts/LeapTestCalc.cs
+++ leanity/Assets/Leanity/Scripts/LeapTestCalc.cs
@@ -2,89 +2,168 @@
 using System.Collections;
 using Leap;
 
+
 public class LeapTestCalc : MonoBehaviour {
+	class HandData
+	{
+		public Vector3 InitialPosition { get; private set; }
+		public Quaternion InitialRotation { get; private set; }
+
+		private Vector3 _position;
+		public Vector3 Position
+		{
+			get { return _position; }
+			set
+			{
+				_position = _positionFilter.Filter(value);
+			}
+		}
+
+		public Quaternion _rotation;
+		public Quaternion Rotation
+		{
+			get { return _rotation;  }
+			set
+			{
+				_rotation = _rotationFilter.Filter(value);
+			}
+		}
+
+		public Vector3 DeltaPosition
+		{
+			get { return Position - InitialPosition; }
+		}
+
+		public Quaternion DeltaRotation
+		{
+			get { return Quaternion.Inverse(InitialRotation) * Rotation; }
+		}
+
+
+		public float grabValue;
+		public bool detected;
+
+		private OneEuroFilter<Quaternion> _rotationFilter;
+		private OneEuroFilter<Vector3> _positionFilter;
+
+		public HandData(float filterFrequency)
+		{
+			_rotationFilter = new OneEuroFilter<Quaternion>(filterFrequency);
+			_positionFilter = new OneEuroFilter<Vector3>(filterFrequency);
+		}
+
+		public void CaptureInitialPose()
+		{
+			InitialPosition = Position;
+			InitialRotation = Rotation;
+		}
+
+		public void SetRotationFilterParams (float frequency, float minCutOff, float beta, float dCutOff)
+		{
+			_rotationFilter.UpdateParams(frequency, minCutOff, beta, dCutOff);
+		}
+
+		public void SetPositionFilterParams (float frequency, float minCutOff, float beta, float dCutOff)
+		{
+			_positionFilter.UpdateParams(frequency, minCutOff, beta, dCutOff);
+		}
+	}
+
+	HandData mainHand;
+	HandData auxHand;
+
 	public KeyCode key = KeyCode.A;
 	Controller c;
 	bool isHolding = false;
-	Quaternion initialObjectRot;
-	Quaternion initialHandRot;
-	Vector3 initialObjectPos;
-	Vector3 initialHandPos;
 
-	Quaternion curHandRotation;
-	Vector3 curHandPosition;
+	Quaternion initialObjectRot;
+	Vector3 initialObjectPos;
+
+	[Header("Sensitivity")]
+
 	[Range(0f, 1f)]
 	public float posScale = 1f;
 
 	[Range(0f, 1f)]
 	public float rotScale = 1f;
 
-	[Range(0f, 1f)]
-	public float rotScaleX = 1f;
+	public Vector3 perAxisRotationScale = Vector3.one;
 
-	[Range(0f, 1f)]
-	public float rotScaleY = 1f;
-
-	[Range(0f, 1f)]
-	public float rotScaleZ = 1f;
-
+	[Header("Operation Mode")]
 
 	public bool isCamera = false;
-
 	public bool invertAxis = false;
+
 	[Range(0f, 1f)]
 	public float grabThreshold = .5f;
 	public bool grabEnabled = false;
 
 	public bool absoluteMovement = true;
+	public bool twoHands = false;
 
 	[Header("Filter")]
 	public bool stabilizePosition = true;
-	[Range(0f, 1f)]
-	public float positionFilterValue = 0.5f;
-	[Range(0f, 1f)]
-	public float rotationFilterValue = 0.5f;
+	public bool stabilizeRotation = true;
 
-	private float _grabValue = 0f;
+	public float filterFrequency = 120f;
 
 	[Header("RotationFilter")]
-	public float rotFilterFrequency = 120.0f;
 	public float rotFilterMinCutoff = 1.0f;
 	public float rotFilterBeta = 0.0f;
 	public float rotFilterDcutoff = 1.0f;
 
 	[Header("PositionFilter")]
-	public float posFilterFrequency = 120.0f;
 	public float posFilterMinCutoff = 1.0f;
 	public float posFilterBeta = 0.0f;
 	public float posFilterDcutoff = 1.0f;
 
-	private OneEuroFilter<Quaternion> rotationFilter;
-	private OneEuroFilter<Vector3> positionFilter;
-
 
 	void Start () {
+		// Initialize tracking
 		c = new Controller();
-		rotationFilter = new OneEuroFilter<Quaternion>(rotFilterFrequency);
-		positionFilter = new OneEuroFilter<Vector3>(posFilterFrequency);
+
+		//Create Hands
+		mainHand = new HandData(filterFrequency);
+		auxHand = new HandData(filterFrequency);
+
+		OnValidate();
+	}
+
+
+	void UpdateHandData(ref Hand leapHand, ref HandData customHand)
+	{
+		customHand.Rotation = lpToUnityRot(leapHand.Rotation);
+		customHand.Position = lpToUnityVec(leapHand.PalmPosition);
+		customHand.grabValue = leapHand.GrabStrength;
+		customHand.detected = true;
+	}
+
+	void UpdateTracking()
+	{
+		Frame frame = c.Frame();
+		// Being pesimistic to avoid some conditionals
+		mainHand.detected = false;
+		auxHand.detected = false;
+
+		// TODO: Check coherence between left/right and hand ids
+		if (frame.Hands.Count > 0)
+		{
+			Hand h = frame.Hands[0];
+			UpdateHandData(ref h, ref mainHand);
+		}
+
+		if (frame.Hands.Count > 1)
+		{
+			Hand h = frame.Hands[1];
+			UpdateHandData(ref h, ref auxHand);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Frame frame = c.Frame();
-		Hand h;
-		if(frame.Hands.Count > 0) {
-			h = frame.Hands[0];
-
-			curHandRotation = lpToUnityRot(h.Rotation);
-			curHandPosition = lpToUnityVec(h.PalmPosition);
-			_grabValue = h.GrabStrength;
-
-		}
-
+		UpdateTracking();
 		KeyController();
 	}
-
 
 	Vector3 lpToUnityVec(Vector lv) {
 		float invertValue = invertAxis ? -1f : 1f;
@@ -92,7 +171,6 @@ public class LeapTestCalc : MonoBehaviour {
 		float scaleFactorY = 0.001f * invertValue;
 		float scaleFactorZ = -0.001f * invertValue;
 		return new Vector3(lv.x * scaleFactorX, lv.y * scaleFactorY, lv.z * scaleFactorZ);
-
 	}
 	Quaternion lpToUnityRot(LeapQuaternion lq) {
 		float invertValue = invertAxis ? -1f : 1f;
@@ -100,12 +178,19 @@ public class LeapTestCalc : MonoBehaviour {
 	}
 
 	void StartMoving() {
-		//Debug.Log("holding!");
-		initialHandPos = curHandPosition;
-		initialObjectPos = transform.position;
+		mainHand.CaptureInitialPose();
+		auxHand.CaptureInitialPose();
+	}
 
-		initialHandRot = curHandRotation;
-		initialObjectRot = transform.rotation;
+
+	private Vector3 NormalizedEulerAngles (Quaternion q)
+	{
+		Vector3 euler = q.eulerAngles;
+		euler.x = NormalizeAngle(euler.x);
+		euler.y = NormalizeAngle(euler.y);
+		euler.z = NormalizeAngle(euler.z);
+
+		return euler;
 	}
 
 	private float NormalizeAngle(float angle)
@@ -117,58 +202,65 @@ public class LeapTestCalc : MonoBehaviour {
 		return angle;
 	}
 
-	void DoMove() {
-		Vector3 deltaMovement = (curHandPosition - initialHandPos) * posScale;
-		if(isCamera) {
+	private void OnValidate()
+	{
+		mainHand?.SetRotationFilterParams(filterFrequency, rotFilterMinCutoff, rotFilterBeta, rotFilterDcutoff);
+		mainHand?.SetPositionFilterParams(filterFrequency, posFilterMinCutoff, posFilterBeta, posFilterDcutoff);
+
+		auxHand?.SetRotationFilterParams(filterFrequency, rotFilterMinCutoff, rotFilterBeta, rotFilterDcutoff);
+		auxHand?.SetPositionFilterParams(filterFrequency, posFilterMinCutoff, posFilterBeta, posFilterDcutoff);
+	}
+
+	void OneHandMove() {
+		Vector3 deltaMovement = mainHand.DeltaPosition * posScale;
+
+		if (isCamera)
+		{
 			deltaMovement = transform.rotation * deltaMovement;
 		}
-		Vector3 targetPosition = absoluteMovement ? initialObjectPos + deltaMovement : transform.position + deltaMovement;
-		//Vector3 stablePosition = Vector3.Lerp(transform.position, targetPosition, positionFilterValue);
-		positionFilter.UpdateParams(posFilterFrequency, posFilterMinCutoff, posFilterBeta, posFilterDcutoff);
-		Vector3 stablePosition = positionFilter.Filter(targetPosition);
 
-		transform.position = stabilizePosition ? stablePosition : targetPosition;
-		Quaternion deltaRot = Quaternion.Inverse( initialHandRot ) * curHandRotation;
-		/*for(int i=1; i<rotScale; i++) {
-			deltaRot *= deltaRot;
-		}*/
-		Vector3 vDeltaRot = deltaRot.eulerAngles;
-		vDeltaRot.x = NormalizeAngle(vDeltaRot.x) * rotScale * rotScaleX;
-		vDeltaRot.y = NormalizeAngle(vDeltaRot.y) * rotScale * rotScaleY;
-		vDeltaRot.z = NormalizeAngle(vDeltaRot.z) * rotScale * rotScaleZ;
+		transform.position = absoluteMovement ? initialObjectPos + deltaMovement : transform.position + deltaMovement;
 
-		// * rotScale;
-		deltaRot = Quaternion.Euler(vDeltaRot);
-		GraphDbg.Log("x", vDeltaRot.x);
-		GraphDbg.Log("y", vDeltaRot.y);
-		Debug.Log(vDeltaRot);
+
+		Quaternion deltaRot = mainHand.DeltaRotation;
+
+		Vector3 eulerDeltaRot = NormalizedEulerAngles(deltaRot);
+		eulerDeltaRot.Scale(perAxisRotationScale);
+		eulerDeltaRot *= rotScale;
+
+		deltaRot = Quaternion.Euler(eulerDeltaRot);
 
 
 		if(isCamera) {
 			Quaternion targetRotation = absoluteMovement? initialObjectRot * deltaRot : transform.rotation * deltaRot;
 
-			rotationFilter.UpdateParams(rotFilterFrequency, rotFilterMinCutoff, rotFilterBeta, rotFilterDcutoff);
-			transform.rotation = rotationFilter.Filter(targetRotation);
-			//transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationFilterValue);
-			Vector3 vRots = transform.rotation.eulerAngles;
-			//vRots.x = vRots.x * rotScale;
+			// Disable Z rotation
+			Vector3 vRots = targetRotation.eulerAngles;
 			vRots.z = 0f;
-			//vRots.y = vRots.y * rotScale;
 			transform.rotation = Quaternion.Euler(vRots);
+
 		} else {
 			transform.rotation = deltaRot * initialObjectRot;
 		}
-
-
 	}
 
 	void StopMoving() {
-		//Debug.Log("unholding");
 	}
 
 	void KeyController() {
-		bool triggerOn = grabEnabled ? _grabValue >= grabThreshold : Input.GetKeyDown(key);
-		bool triggerOff = grabEnabled ? _grabValue < grabThreshold : Input.GetKeyUp(key);
+		bool isGrabbing = false;
+		if (twoHands)
+		{
+			isGrabbing = mainHand.detected && mainHand.grabValue >= grabThreshold &&
+			             auxHand.detected && auxHand.grabValue >= grabThreshold;
+		} else
+		{
+			isGrabbing = mainHand.detected && mainHand.grabValue >= grabThreshold;
+		}
+
+
+		bool triggerOn = grabEnabled && isGrabbing || Input.GetKeyDown(key);
+		bool triggerOff = !triggerOn;
 
 		if(!isHolding && triggerOn ) {
 			isHolding = true;
@@ -179,7 +271,7 @@ public class LeapTestCalc : MonoBehaviour {
 		}
 
 		if(isHolding) {
-			DoMove();
+			OneHandMove();
 		}
 	}
 }
