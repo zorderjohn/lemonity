d787d77f88e6e222652de157c29560a99b955e37
Jonatan Martinez <jonatan.m@gmail.com>
2019-09-22 13:26:01 +0200

    Scale motion follows center of the gesture

--- leanity/Assets/Leanity/Scripts/MotionStyles/ScaleMotion.cs
+++ leanity/Assets/Leanity/Scripts/MotionStyles/ScaleMotion.cs
@@ -8,8 +8,9 @@ namespace Leanity
 
 		private float _initialLogScale;
 
-		private Vector3 _initialRelativeVector;
 		private Vector3 _wcInitialPosition;
+		private Vector3 _hcCenterInitialPos;
+		private Vector3 _ccInitialCamToCenter;
 
 		protected override void StartMotion()
 		{
@@ -19,7 +20,8 @@ namespace Leanity
 			_initialLogScale = MathHelper.LinearToLogScale(Scale);
 
 			_wcInitialPosition = Position;
-			_initialRelativeVector = Rotation * HandTracking.CamToHandOffset(Scale);
+			_hcCenterInitialPos = (LeftGesture.HandCurrentPosition + RightGesture.HandCurrentPosition) * 0.5f;
+			_ccInitialCamToCenter = HandTracking.CamToHandOffset(Scale) + Scale * _hcCenterInitialPos;
 		}
 
 		protected override void UpdateMotion()
@@ -32,10 +34,11 @@ namespace Leanity
 			float currentLogScale = Mathf.Clamp(_initialLogScale + sepDelta, 0f, 10f);
 			Scale = MathHelper.LogToLinearScale(currentLogScale);
 
-			var finalRelativeVector = Rotation * HandTracking.CamToHandOffset(Scale);
-			var deltaVector = -finalRelativeVector + _initialRelativeVector;
+			var hcCenterFinalPos = (LeftGesture.HandCurrentPosition + RightGesture.HandCurrentPosition) * 0.5f;
+			var ccFinalCamToCenter = HandTracking.CamToHandOffset(Scale) + Scale * hcCenterFinalPos;
+			var deltaVector = _ccInitialCamToCenter - ccFinalCamToCenter;
 
-			Position = _wcInitialPosition + deltaVector;
+			Position = _wcInitialPosition + Rotation * deltaVector;
 		}
 	}
 }
